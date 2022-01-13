aa0a6ae2b60b6965e964b98bc976d3592e72112a
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-01-13 08:07:18 -0600

    initial commit for values and variables.

---
+++ script.js
@@ -0,0 +1,44 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Jonas";
+// let PI = 3.14125
+
+// //good variable names
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+
+// //bad variable names
+// let job1 = 'programmer';
+// let job2 = 'teahcer';
+
+// //console.log print statement
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// declaring a variable allows you to change information in multiple places at once
+
+//camelCase is the standard form of naming in JS
+
+// cannot start a variable name with an integer
+
+// variable names can only contain numbers, letters underscores, or the dollar sign.
+
+// variable names cannot be js reserved keywords (name is somewhat reserved)
+
+// variable names be lowercase unless declaring a class (OOP)
+
+// Variables in all caps are reserved for variables we know will be constant IE. let PI = 3.14125
+
+// variable names should be clear and accurate
+
+
+
+
+
